a1778949e02148898aadee4b11d0cbb634a7a029
Robert Friemer <bugs@friemmedia.de>
2023-02-05 11:22:56 +0100

    Upload input-examples for php-Languages.

---
+++ input-examples/serv2pf-redirect.php
@@ -0,0 +1,18 @@
+<?php
+
+# Get the Client IP-Address
+$ip = isset($_SERVER['HTTP_CLIENT_IP']) 
+    ? $_SERVER['HTTP_CLIENT_IP'] 
+    : (isset($_SERVER['HTTP_X_FORWARDED_FOR']) 
+      ? $_SERVER['HTTP_X_FORWARDED_FOR'] 
+      : $_SERVER['REMOTE_ADDR']);
+
+# Open the Text-File, write the IP-Address and close it again.
+$s2pfile = fopen("/tmp/ip-liste.txt", "a");
+fwrite($s2pfile, $ip."\n");
+fclose($s2pfile);
+
+# Execute the Redirect the the error-Page
+echo "<script>location.href='error.html';</script>";
+
+?>
